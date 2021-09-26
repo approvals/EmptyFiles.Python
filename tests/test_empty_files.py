c8bbb322df224815589706f6bfb4961295a6982e
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-09-26 17:25:13 +0000

    t cleanup tests

--- tests/test_empty_files.py
+++ tests/test_empty_files.py
@@ -1,9 +1,26 @@
+import os
+import shutil
+
 from approvaltests import verify_file
 
 from empty_files.empty_files import create_empty_file
 
 
-def test_simple():
-    create_empty_file("temp/empty.txt")
-    verify_file("temp/empty.txt")
+def test_create_base_empty_file():
+    verify_file_creation("temp/empty.txt")
+
+
+def verify_file_creation(file_name):
+    clear_temp()
+    create_empty_file(file_name)
+    verify_file(file_name)
+
+
+def test_create_empty_file_in_nested_directory():
+    verify_file_creation("temp/temp/empty.txt")
+
+
+def clear_temp():
+    if os.path.exists("temp"):
+        shutil.rmtree("temp")
 
