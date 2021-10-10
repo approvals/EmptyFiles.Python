68506b8d03208f2b25e9a190ddc04f4a696c0347
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-10-10 17:47:21 +0000

    *** WIP
    Co-Authored-By: To Be Agile <2531294+tobeagile@users.noreply.github.com>
    Co-Authored-By: AustinAbro321 <37223396+AustinAbro321@users.noreply.github.com>

--- tests/test_empty_files.py
+++ tests/test_empty_files.py
@@ -1,7 +1,8 @@
 import os
 import shutil
+from pathlib import Path
 
-from approvaltests import verify_file
+from approvaltests import verify_file, Options
 
 from empty_files.empty_files import create_empty_file
 
@@ -9,15 +10,20 @@ from empty_files.empty_files import create_empty_file
 def test_create_base_empty_file():
     verify_file_creation("temp/empty.txt")
 
-
-def verify_file_creation(file_name):
-    clear_temp()
-    create_empty_file(file_name)
-    verify_file(file_name)
+def test_create_binary():
+    verify_file_creation("temp/empty.jpg")
 
 
 def test_create_empty_file_in_nested_directory():
     verify_file_creation("temp/temp/empty.txt")
+
+
+def verify_file_creation(file_name):
+    clear_temp()
+    create_empty_file(file_name)
+    extension = Path(file_name).suffixes[0]
+
+    verify_file(file_name, options=Options().for_file.with_extension(extension))
 
 
 def clear_temp():
