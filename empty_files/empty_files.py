f2b00a0053eff60fce7bd921dd347a368efa2cf2
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-10-17 16:43:42 +0000

    F!! Downloads empty files
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- empty_files/empty_files.py
+++ empty_files/empty_files.py
@@ -1,6 +1,28 @@
 from pathlib import Path
+
+import requests as requests
+
+
 def create_empty_file(file_path: str) -> None:
     # create folders if needed
-    Path(file_path).parent.mkdir(parents=True, exist_ok=True)
-    # create empty file using the name
-    open(file_path, "w").close()
+    path = Path(file_path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    extension = path.suffix
+    #download the correct file from the internet
+    try:
+        download_file(f"https://github.com/VerifyTests/EmptyFiles/raw/main/index/empty{extension}", file_path)
+    except:
+        open(file_path, "w").close()
+
+
+def download_file(url: str, file_path: str) -> str:
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk:
+                f.write(chunk)
+    return file_path
